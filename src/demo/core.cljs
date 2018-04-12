074ca9bbecb157a1d4b0f7d252397fd75a73e80b
Enzzo Cavallo <souenzzo@gmail.com>
2018-04-11 23:54:02 -0300

    Initial

---
+++ src/demo/core.cljs
@@ -0,0 +1,31 @@
+(ns demo.core
+    (:require ["graphql-voyager" :as voyager]
+              ["React" :as react]
+              ["ReactDOM" :as react-dom]))
+
+(defn introspection-provider
+  [query]
+  (-> (js/fetch (str (-> js/window .-location .-origin) "/graphql")
+                #js {:method  "post"
+                     :headers #js {:Content-Type "application/json"}
+                     :body    (.stringify js/JSON #js {:query query})})
+      (.then (fn [response] (.json response)))))
+
+
+(enable-console-print!)
+
+(.render react-dom (.createElement react voyager/Voyager #js{:introspection introspection-provider} nil)
+
+
+
+
+(println "This text is printed from src/demo/core.cljs. Go ahead and edit it and see reloading in action.")
+
+;; define your app data so that it doesn't get over-written on reload
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+
+(defn on-js-reload []
+
+)
